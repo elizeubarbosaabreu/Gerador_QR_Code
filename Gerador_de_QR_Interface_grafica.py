0f5afd2b0af60ba4b72001f934076ce849e31fc8
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2021-09-14 20:35:00 -0400

    Update Gerador_de_QR_Interface_grafica.py

--- Gerador_de_QR_Interface_grafica.py
+++ Gerador_de_QR_Interface_grafica.py
@@ -1,17 +1,16 @@
 import PySimpleGUI as sg
-import qrcode
-import pathlib
-import webbrowser
+import qrcode, pathlib, webbrowser
 
-sg.theme('SystemDefault')   # Adicione um tema de sua preferência
-# All the stuff inside your window.
-layout = [  [sg.Text('Digite o conteúdo para converter em QR CODE: '), sg.InputText()],
-            [sg.Text('Digite o nome da imagem. Ex.: "default.png": '), sg.InputText()],
-            [sg.Text('Status:'), sg.Output(size=(80, 1))],
+sg.theme('Dark')   # Adicione um tema de sua preferência
+# Conteúdo da janela.
+layout = [  [sg.Image('image/app.png'), sg.Text('GERADOR DE QR CODE')],
+            [sg.Text('Conte\u00fado para converter em QR CODE: '), sg.InputText(size=(33, 5))],
+            [sg.Text('Nome da imagem a ser gerada. Ex.: "default.png": '), sg.InputText(size=(25, 1))],
+            [sg.Text('Status:'), sg.Output(size=(60, 2))],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
-# Create the Window
-window = sg.Window('GERADOR DE QR CODE', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+# Criação da janela
+window = sg.Window('GERADOR DE QR CODE', layout, element_justification='center')
+# Eventos e processamento de valores
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
@@ -24,7 +23,7 @@ while True:
         nome_arquivo = 'default.png'
     img.save(nome_arquivo)
     caminho = (pathlib.Path(__file__).parent.absolute())
-    print(f'Arquivo "{nome_arquivo.upper()}" gerado com sucesso!!!') 
+    print(f'Arquivo {nome_arquivo.upper()} gerado com sucesso!!!') 
     webbrowser.open(f'{caminho}/{nome_arquivo}')
 
 window.close()
