f6703c195ca7ca433c954ba625981e80498c2663
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2021-09-18 23:16:30 -0400

    Update Gerador_de_QR_Interface_grafica.py

--- Gerador_de_QR_Interface_grafica.py
+++ Gerador_de_QR_Interface_grafica.py
@@ -1,29 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import PySimpleGUI as sg
 import qrcode, pathlib, webbrowser
 
 sg.theme('Reddit')   # Adicione um tema de sua preferência
-# Conteúdo da janela.
-layout = [  [sg.Image('image/app.png'), sg.Text('GERADOR DE QR CODE')],
-            [sg.Text('Conte\u00fado para converter em QR CODE: '), sg.InputText(size=(33, 5))],
-            [sg.Text('Nome da imagem a ser gerada. Ex.: "default.png": '), sg.InputText(size=(25, 1))],
+# Conteúdo da janela
+
+layout = [  [sg.Text('GERADOR DE QR CODE', font=('Arial 20'))],
+            [sg.Text('Conte\u00fado para converter em QR CODE: '), sg.InputText(size=(33, 5), key='-conteudo-')],
+            [sg.Text('Nome da imagem a ser gerada. Ex.: "default.png": '), sg.InputText(size=(25, 1), key='-nome_da_imagem-')],
             [sg.Text('Status:'), sg.Output(size=(60, 2))],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
+
 # Criação da janela
 window = sg.Window('GERADOR DE QR CODE', layout, element_justification='center')
+
 # Eventos e processamento de valores
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    
+    if event == sg.WIN_CLOSED or event == 'Cancel': 
         break
-    conteudo = values[1]
+    
+    conteudo = values['-conteudo-']
     img = qrcode.make(conteudo)
     type(img)
-    nome_arquivo = (values[2])
+    nome_arquivo = (values['-nome_da_imagem-'])
+    
     if nome_arquivo == '':
         nome_arquivo = 'default.png'
+        
     img.save(nome_arquivo)
     caminho = (pathlib.Path(__file__).parent.absolute())
-    print(f'Arquivo {nome_arquivo.upper()} gerado com sucesso!!!') 
+    print(f'Arquivo {nome_arquivo.upper()} gerado com sucesso!!!')
+    print(values['-nome_da_imagem-'])
     webbrowser.open(f'{caminho}/{nome_arquivo}')
 
 window.close()
