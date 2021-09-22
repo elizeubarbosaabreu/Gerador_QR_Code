63631f8c02182bcd5789512d6a311d6b0bad495d
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2021-09-22 19:23:24 -0400

    Update Gerador_de_QR_Interface_grafica.py

--- Gerador_de_QR_Interface_grafica.py
+++ Gerador_de_QR_Interface_grafica.py
@@ -36,7 +36,7 @@ window = sg.Window('GERADOR DE QR CODE', layout)
 while True:
     event, values = window.read()
     
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED: 
         break
     
     conteudo = values['-conteudo-']    
